06a5f122f5ed07f9d3783196b14face62be4b4cd
zele3813 <zeledong3813@gmail.com>
2023-04-24 21:22:38 -0400

    fix navbar

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,15 @@
-// console.log("scripts.js linked");
 const sections = document.querySelectorAll('section');
 const navs = document.querySelectorAll('nav ul li');
-let prev = "home"
-let current = "home";
+let prev = ""
+let current = "";
 
 window.addEventListener('scroll', ()=>{
     sections.forEach(section => {
-        // console.log(section.getAttribute('id'));
         const sectionTop = section.offsetTop;
         const sectionHeight = section.clientHeight;
-        if(scrollY >= (sectionTop - sectionHeight / 3) && scrollY < (sectionTop + sectionHeight / 2)){
+        if(scrollY >= (sectionTop - sectionHeight / 3) && scrollY < (sectionTop + sectionHeight - sectionHeight / 3)){
             prev = current;
-            // console.log(prev)
             current = section.getAttribute('id');
-            // console.log(current)
         }
     })
     if(prev != current){
